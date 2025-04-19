69a2d570a11a83ce686eacac9f74cd7db9ca2fe4
npakk <npakk669@gmail.com>
2025-04-19 11:01:30 +0900

    idをSQL標準に準拠した自動採番構文に変更

--- initdb/setup.sql
+++ initdb/setup.sql
@@ -1,7 +1,7 @@
 create database sinatra_app;
 \c sinatra_app;
 create table memos (
-  id serial PRIMARY KEY,
+  id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   title text NOT NULL,
   body text NOT NULL,
   is_delete boolean NOT NULL
